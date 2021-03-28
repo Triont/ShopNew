b0ab18f2c68eacc337a8fdb82c572c576aee5097
Triont <34381510+Triont@users.noreply.github.com>
2021-03-29 00:47:03 +0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
